b2dff6309a822f49800ed06d9a5d41b4df420d1d
pascual_silvestri <pascualsilvestri14@gmail.com>
2023-04-20 14:38:56 -0300

    commit

--- apps/afiliado/models.py
+++ apps/afiliado/models.py
@@ -3,12 +3,23 @@ from django.conf import settings
 
 
 class Afiliado(models.Model):
-    idAfiliado = models.TextField(max_length=50)
-    nombre = models.TextField(max_length=200)
+    idAfiliado = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=200)
     url = models.URLField()
     
     def __str__(self):
         return self.nombre
     
     
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    correo = models.EmailField()
+    telefono = models.IntegerField()
+    comprobante = models.ImageField(upload_to='comprobante')
+    idAfiliado = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.nombre
     

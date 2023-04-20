4ec1f83eac0c71a85accbb829b9c32a2d2175b92
Ciscode <vignardel.francisco@gmail.com>
2023-04-19 22:20:43 -0300

    formulario

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+function send_handle(){
+
+  let nombre=document.getElementById("nombre").value;
+
+  let apellido= document.getElementById("apellido").value;
+
+  let userTelegram= document.getElementById("userTelegram").value;
+
+  let email= document.getElementById("email").value;
+
+  let telefono= document.getElementById("telefono").value;
+
+  
+  var win = window.open(`https://wa.me/1138658887?text=Mi%20nombre%20es%20${nombre}%20${apellido}%0AMi%20usuario%20de%20Telegram%20es%20${userTelegram}%0AMi%20email%20es%20${email}%0AMi%20telefono%20es%20${telefono}`, '_blank');
+  win.focus();
+}

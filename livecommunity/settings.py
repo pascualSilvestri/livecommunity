a995dfef421c585826dc053203dceafe0433b447
pascual_silvestri <pascualsilvestri14@gmail.com>
2023-05-05 12:56:15 -0300

    Update settings.py

--- livecommunity/settings.py
+++ livecommunity/settings.py
@@ -132,28 +132,28 @@ DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
-#Database
-#https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# ALLOWED_HOSTS = ['31.220.21.60','localhost']
-# CSRF_TRUSTED_ORIGINS = ['https://livecommunity.com']
+# ALLOWED_HOSTS = []
+# #Database
+# #https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'livecommunity',
-#         'USER': 'root',
-#         'PASSWORD': 'livecommunity01',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+ALLOWED_HOSTS = ['31.220.21.60','localhost']
+CSRF_TRUSTED_ORIGINS = ['https://livecommunity.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'livecommunity',
+        'USER': 'root',
+        'PASSWORD': 'livecommunity01',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}

7dbcef310d42a6250fcfc297a58697bf9c057b59
pascual_silvestri <pascualsilvestri14@gmail.com>
2023-08-13 19:57:03 -0300

    Update settings.py

--- livecommunity/settings.py
+++ livecommunity/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'apps.usuarios',
     'apps.afiliado',
     'apps.api'
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'livecommunity.urls'

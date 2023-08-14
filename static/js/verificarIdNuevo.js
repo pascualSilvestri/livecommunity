ab70620a3eb4576369f329a3356457f0cb7b4a6e
pascual_silvestri <pascualsilvestri14@gmail.com>
2023-08-13 22:23:54 -0300

    validaciones

--- static/js/verificarIdNuevo.js
+++ static/js/verificarIdNuevo.js
@@ -31,7 +31,8 @@ async function obtenerDatos() {
       
         //Local http://127.0.0.1:8000/verificar/
         //produc https://livecommunity.info/verificar/
-      const response = await fetch('http://127.0.0.1:8000/api/verificar/'); // cambiar a localhost para local
+        //https://livecommunity.info/api/verificar/
+      const response = await fetch('https://livecommunity.info/api/verificar/'); // cambiar a localhost para local
       if (!response.ok) {
         throw new Error('Error en la respuesta del servidor');
       }

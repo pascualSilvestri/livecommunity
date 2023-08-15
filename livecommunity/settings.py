95bc3e1ffd85b651a99b414405e5c6a36d9a540b
Ciscode <vignardel.francisco@gmail.com>
2023-08-15 18:41:29 -0300

    cambios

--- livecommunity/settings.py
+++ livecommunity/settings.py
@@ -171,24 +171,24 @@ CORS_ALLOW_METHODS = [
 
 
 
-# TELEGRAM_BOT_TOKEN = '6154942852:AAGfHB6dNhTOxc0gwg-Qnop4LnnMVf9jr8c'
-# CHAT_ID_BOT = '@pruebapas'
+TELEGRAM_BOT_TOKEN = '6154942852:AAGfHB6dNhTOxc0gwg-Qnop4LnnMVf9jr8c'
+CHAT_ID_BOT = '@pruebapas'
 
-# DEBUG = True
+DEBUG = True
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
 
 
-# ALLOWED_HOSTS = []
-#Database
-#https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+ALLOWED_HOSTS = []
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 ####################################################
@@ -197,24 +197,24 @@ CORS_ALLOW_METHODS = [
 #
 
 
-TELEGRAM_BOT_TOKEN = '6112305386:AAFFJNJp-mZy-XKT0JACkw0LEhvp3_knxlk'
-CHAT_ID_BOT = '@liveingresos'
+# TELEGRAM_BOT_TOKEN = '6112305386:AAFFJNJp-mZy-XKT0JACkw0LEhvp3_knxlk'
+# CHAT_ID_BOT = '@liveingresos'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-DEBUG = False
+# DEBUG = False
 
 
-ALLOWED_HOSTS = ['livecommunity.info','31.220.21.60','localhost','livecommunity.xyz','www.livecommunity.xyz']
-CSRF_TRUSTED_ORIGINS = ['https://livecommunity.com','https://www.livecommunity.xyz','https://livecommunity.xyz']
+# ALLOWED_HOSTS = ['livecommunity.info','31.220.21.60','localhost','livecommunity.xyz','www.livecommunity.xyz']
+# CSRF_TRUSTED_ORIGINS = ['https://livecommunity.com','https://www.livecommunity.xyz','https://livecommunity.xyz']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'livecommunity',
-        'USER': 'root',
-        'PASSWORD': 'live2023community01',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'livecommunity',
+#         'USER': 'root',
+#         'PASSWORD': 'live2023community01',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }

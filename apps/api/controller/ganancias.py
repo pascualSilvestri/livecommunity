b576e04469f7e6a7168f0b852ed736afd43c01ff
pascual_silvestri <pascualsilvestri14@gmail.com>
2023-09-06 10:44:39 -0300

    Update ganancias.py

--- apps/api/controller/ganancias.py
+++ apps/api/controller/ganancias.py
@@ -2,7 +2,6 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
 from ...utils.formulas import calcula_porcentaje_directo,calcular_porcentaje_indirecto
-from ...utils.funciones import formatera_retiro
 from ...usuarios.models import Spread,Usuario,Cuenta,BonoAPagar
 from ...api.models import Registros_ganancias,Registros_cpa
 import re

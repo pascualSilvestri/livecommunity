039ceec5f8856ed1fe48ea1b64626f7f5d8f83de
Cisco <69130363+franvigna@users.noreply.github.com>
2023-12-07 13:09:27 -0300

    slider about & slider staff

--- static/js/slider.js
+++ static/js/slider.js
@@ -5,7 +5,7 @@ const arrowBtns = document.querySelectorAll('.wrapper i')
 const carouselChildrens = [...carousel.children]
 
 let isDragging = false,
-    isAutoPlay = true,
+    isAutoPlay = false,
     startX,
     startScrollLeft,
     timeoutId
@@ -13,17 +13,21 @@ let isDragging = false,
 // Get the number of cards that can fit in the carousel at once
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth)
 
-// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+// Insert copies of the last few cards to the beginning of the carousel for infinite scrolling
 carouselChildrens
     .slice(-cardPerView)
     .reverse()
-    .forEach((card) => {
-        carousel.insertAdjacentHTML('afterbegin', card.outerHTML)
+    .forEach((card, index) => {
+        const clonedCard = card.cloneNode(true)
+        clonedCard.classList.add('infinite-card', `infinite-card-${index}`)
+        carousel.insertAdjacentElement('afterbegin', clonedCard)
     })
 
-// Insert copies of the first few cards to end of carousel for infinite scrolling
-carouselChildrens.slice(0, cardPerView).forEach((card) => {
-    carousel.insertAdjacentHTML('beforeend', card.outerHTML)
+// Insert copies of the first few cards to the end of the carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach((card, index) => {
+    const clonedCard = card.cloneNode(true)
+    clonedCard.classList.add('infinite-card', `infinite-card-${index}`)
+    carousel.insertAdjacentElement('beforeend', clonedCard)
 })
 
 // Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox

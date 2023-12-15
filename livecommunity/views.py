8b0ff186752393aa7a155264846115777bbacefe
Cisco <69130363+franvigna@users.noreply.github.com>
2023-12-15 11:18:42 -0300

    broker, presenciales y servicios comment

--- livecommunity/views.py
+++ livecommunity/views.py
@@ -3,11 +3,23 @@ from django.shortcuts import render
 def Home(request):
     return render(request,'index.html')
 
+
+#PRIMER SPRIG   
 def Broker(request):
-    return render(request,'broker.html')    
+    return render(request,'inprocess.html')    
 
 def Presenciales(request):
-    return render(request,'presenciales.html')    
+    return render(request,'inprocess.html')    
 
 def Servicios(request):
-    return render(request,'servicios.html')    
+    return render(request,'inprocess.html')    
+
+
+# def Broker(request):
+#     return render(request,'broker.html')    
+
+# def Presenciales(request):
+#     return render(request,'presenciales.html')    
+
+# def Servicios(request):
+#     return render(request,'servicios.html')    

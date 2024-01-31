08034215d5d8478370551b44e159b4dd2d2c2c42
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-01-31 19:32:44 -0300

    Update admin.py

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Relation_fpa_client,Registro_archivo,Registros_cpa,Registros_ganancias,SpreadIndirecto
+from .models import Relation_fpa_client,Registro_archivo,Registros_cpa,Registros_ganancias,SpreadIndirecto,Cpa_a_pagar
 # Register your models here.
 admin.site.register(Relation_fpa_client)
 admin.site.register(Registro_archivo)
 admin.site.register(Registros_cpa)
 admin.site.register(Registros_ganancias)
 admin.site.register(SpreadIndirecto)
+admin.site.register(Cpa_a_pagar)
 

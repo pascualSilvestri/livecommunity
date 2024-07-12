cf8c478c8f866233647bb2a3b657ea46eb53c3c9
franvigna <69130363+franvigna@users.noreply.github.com>
2024-07-12 11:34:30 -0300

    animation loading tittle

--- static/js/animations/headerAnimationLanding.js
+++ static/js/animations/headerAnimationLanding.js
@@ -1,17 +1,28 @@
-const navbar = document.querySelector("#navBar");
-const heightDisplay = window.innerHeight;
-const widthDisplay = window.innerWidth;
-const navbarDisplay = navbar.getBoundingClientRect().height;
+const navbar = document.querySelector('#navBar')
+const heightDisplay = window.innerHeight
+const widthDisplay = window.innerWidth
+const navbarDisplay = navbar.getBoundingClientRect().height
 
-window.addEventListener("scroll", () => {});
+window.addEventListener('scroll', () => {})
 
-document.addEventListener("DOMContentLoaded", () => {
-  const loading = document.querySelector(".loading");
+document.addEventListener('DOMContentLoaded', () => {
+    const loading = document.querySelector('.loading')
 
+    window.addEventListener('load', () => {
+        setTimeout(() => {
+            loading.style.display = 'none'
 
-  window.addEventListener("load", () => {
-    setTimeout(() => {
-      loading.style.display = "none";
-    }, 1000);
-  });
-});
+            // Animación del título y el botón
+            const title = document.querySelector('.hero h1')
+            const button = document.querySelector('.hero .btn')
+
+            setTimeout(() => {
+                title.classList.add('animate')
+            }, 500) // 500 ms de retardo para el título
+
+            setTimeout(() => {
+                button.classList.add('animate')
+            }, 1000) // 1000 ms de retardo para el botón
+        }, 1000)
+    })
+})

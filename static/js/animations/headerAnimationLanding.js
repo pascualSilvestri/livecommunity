e5e5d854d9a1f25b326dd4ed0319dba3c5c63a50
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-07-12 11:27:18 -0300

    the component loading is created

--- static/js/animations/headerAnimationLanding.js
+++ static/js/animations/headerAnimationLanding.js
@@ -1,22 +1,17 @@
-const navbar = document.querySelector('#navBar');
+const navbar = document.querySelector("#navBar");
 const heightDisplay = window.innerHeight;
 const widthDisplay = window.innerWidth;
 const navbarDisplay = navbar.getBoundingClientRect().height;
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {});
 
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    const loading = document.createElement('div');
-    loading.classList.add('loading');
-    loading.textContent = 'Loading...';
-    document.body.appendChild(loading);
+document.addEventListener("DOMContentLoaded", () => {
+  const loading = document.querySelector(".loading");
 
 
-    window.addEventListener('load', () => {
-        setTimeout(() => {
-            loading.remove();
-        }, 1000);
-    });
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      loading.style.display = "none";
+    }, 1000);
+  });
 });

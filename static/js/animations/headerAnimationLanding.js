68877fca355d3fe1504e5eb3c628338e907d2f51
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-07-12 10:44:43 -0300

    modificaciones en el hero

--- static/js/animations/headerAnimationLanding.js
+++ static/js/animations/headerAnimationLanding.js
@@ -1,2 +1,22 @@
-const hero = document.querySelector('.hero');
+const navbar = document.querySelector('#navBar');
+const heightDisplay = window.innerHeight;
+const widthDisplay = window.innerWidth;
+const navbarDisplay = navbar.getBoundingClientRect().height;
 
+window.addEventListener('scroll', () => {
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loading = document.createElement('div');
+    loading.classList.add('loading');
+    loading.textContent = 'Loading...';
+    document.body.appendChild(loading);
+
+
+    window.addEventListener('load', () => {
+        setTimeout(() => {
+            loading.remove();
+        }, 1000);
+    });
+});

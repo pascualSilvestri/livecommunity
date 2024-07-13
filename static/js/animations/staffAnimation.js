73f31be7141d404915e0ac80a2b9ef2ddd0384b2
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-07-13 11:10:52 -0300

    cambiando el stylo de staff

--- static/js/animations/staffAnimation.js
+++ static/js/animations/staffAnimation.js
@@ -1,30 +1,45 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const title = document.querySelector('.tus_maestros h2')
-    const text = document.querySelector('.tus_maestros p')
-    const section = document.querySelector('.staff')
+document.addEventListener("DOMContentLoaded", () => {
+  const title = document.querySelector(".tus_maestros h2");
+  const text = document.querySelector(".tus_maestros p");
+  const section = document.querySelector(".staff");
+  const cardStaff = document.querySelectorAll(".cardStaff");
+  
 
-    // Verificar que los elementos existan
-    if (title && text && section) {
-        const observerOptions = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.1,
+  // Verificar que los elementos existan
+  if (title && text && section) {
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          title.classList.add("animate");
+          text.classList.add("animate");
+          observer.disconnect(); // Deja de observar una vez que la animación se ha activado
         }
+      });
+    }, observerOptions);
 
-        const observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    title.classList.add('animate')
-                    text.classList.add('animate')
-                    observer.disconnect() // Deja de observar una vez que la animación se ha activado
-                }
-            })
-        }, observerOptions)
+    observer.observe(section);
+  } else {
+    console.error(
+      "No se encontraron los elementos necesarios para las animaciones."
+    );
+  }
 
-        observer.observe(section)
-    } else {
-        console.error(
-            'No se encontraron los elementos necesarios para las animaciones.'
-        )
-    }
-})
+  cardStaff.forEach((card) =>
+    card.addEventListener("mouseenter", () => {
+        card.querySelector('ul').classList.add('show');
+    })
+  );
+
+  cardStaff.forEach((card) =>
+    card.addEventListener("mouseleave", () => {
+        card.querySelector('ul').classList.remove('show');
+    })
+  );
+
+});

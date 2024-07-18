8853c49cf140a30ca2f6432f06cf2bcee30148bc
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-07-18 13:49:44 -0300

    agregando animacion a la seccion de mision y vision

---
+++ static/js/animations/misionVisionAnimation.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('.mision_vision_container')
+    const mision_vision_container = document.querySelector('.mision_vision_container')
+    const stats_container = document.querySelector('.stats_container')
+    const titleAbout = document.querySelector('.about_us_h2')
+    const textContainer = document.querySelector('.about_us_p')
+
+    if (!container) {
+        console.error('No se encontró el contenedor con la clase .about-us')
+        return
+    }
+
+    const containerTop = container.getBoundingClientRect().top + window.scrollY
+
+    window.addEventListener('scroll', () => {
+        const scrollPosition = window.scrollY
+
+        console.log('Posición de scroll:', scrollPosition)
+        console.log('Posición superior del contenedor:', containerTop)
+
+        if (scrollPosition >= containerTop - 300) {
+            mision_vision_container.classList.add('animate')
+            setTimeout(() => {
+                stats_container.classList.add('animate')
+            }, 500) // Ajusta el tiempo de retraso según lo necesites
+            setTimeout(() => {
+                titleAbout.classList.add('animate')
+            }, 500) // Ajusta el tiempo de retraso según lo necesites
+            setTimeout(() => {
+                textContainer.classList.add('animate')
+            }, 500) // Ajusta el tiempo de retraso según lo necesites
+        } else {
+            mision_vision_container.classList.remove('animate')
+            stats_container.classList.remove('animate')
+            titleAbout.classList.remove('animate')
+            textContainer.classList.remove('animate')
+        }
+
+        // Efecto parallax para las imágenes
+        if (mision_vision_container.classList.contains('animate')) {
+            mision_vision_container.style.transform = `translateY(${
+                scrollPosition * 0.1
+            }px)`
+        }
+        if (stats_container.classList.contains('animate')) {
+            stats_container.style.transform = `translateY(${
+                scrollPosition * 0.1
+            }px)`
+        }
+    })
+})

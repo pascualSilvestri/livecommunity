aad8aa9e3527d07265ceb9b5ba4bb3798c2fec63
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-07-22 08:49:56 -0300

    agregando animaciones a testimonios

--- static/js/animations/testimonioAnimationLanding.js
+++ static/js/animations/testimonioAnimationLanding.js
@@ -1,8 +1,27 @@
-const slider = document.querySelector('.slider')
-const slides = document.querySelectorAll('.slide')
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.querySelector('.slider');
+    const slides = document.querySelectorAll('.slide');
 
-// Clonar las slides para efecto infinito
-slides.forEach((slide) => {
-    const clone = slide.cloneNode(true)
-    slider.appendChild(clone)
-})
+    // Clonar las slides para efecto infinito
+    slides.forEach((slide) => {
+        const clone = slide.cloneNode(true);
+        slider.appendChild(clone);
+    });
+
+    slides.forEach((slide) => {
+        slide.addEventListener('mouseenter', () => {
+            slider.style.animationPlayState = 'paused';
+            slide.style.transform = 'scale(1.5)';
+            slide.style.transition = 'transform 0.5s ease';
+            slide.style.backgroundColor = '#031542';
+            slide.style.zIndex = '10';
+        });
+
+        slide.addEventListener('mouseleave', () => {
+            slider.style.animationPlayState = 'running';
+            slide.style.transform = 'scale(1)';
+            slide.style.backgroundColor = '#06102bb9';
+            slide.style.zIndex = '0';
+        });
+    });
+});

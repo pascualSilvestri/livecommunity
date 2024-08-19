96917e0caaaa345c3567e98f69f7f557b213e122
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-08-19 17:35:01 -0300

    listo pagina de regitro

--- static/js/verificarIdNuevo.js
+++ static/js/verificarIdNuevo.js
@@ -14,7 +14,7 @@ async function obtenerDatos() {
       
         //http://127.0.0.1:8000/api/verificar/
         //https://livecommunity.info/api/verificar/
-      const response = await fetch('https://livecommunity.info/api/verificar/'); // cambiar a localhost para local
+      const response = await fetch('http://127.0.0.1:8000/api/verificar/'); // cambiar a localhost para local
       if (!response.ok) {
         throw new Error('Error en la respuesta del servidor');
       }

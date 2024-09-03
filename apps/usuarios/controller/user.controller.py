0c29e1a2d6f60cd8ac0c95563e63ee03672b9947
pascualSilvestri <85641129+pascualSilvestri@users.noreply.github.com>
2024-09-03 17:49:40 -0300

    c

---
+++ apps/usuarios/controller/user.controller.py
@@ -0,0 +1,48 @@
+
+@csrf_exempt
+def postNewUser(request):
+    if request.method == 'POST':
+        if 'application/json' in request.content_type:
+            try:
+                # Decodificar el cuerpo de la solicitud como JSON
+                data = json.loads(request.body)
+                usuario = Usuario.objects.all()
+            
+                afiliados = Afiliado.objects.filter(fpa=data.get('fpa'))
+                
+                if afiliados.exists():
+                    print(afiliados[0].fpa)
+                    uplink = afiliados[0].upline
+                    link=afiliados[0].url 
+                # Crear un nuevo usuario y guardar los datos en la base de datos
+                
+                    new_user = Usuario(
+                        username = (data.get('fpa') + "_" + data.get('first_name')).replace(' ', '_'),
+                        fpa=data.get('fpa'),
+                        email=data.get('email'),
+                        first_name=data.get('first_name'),
+                        last_name=data.get('last_name'),
+                        password=data.get('password'),
+                        telephone=data.get('telephone'),
+                        wallet=data.get('wallet'),
+                        uplink=uplink or '',
+                        link=link or ''
+                    )
+                    new_user.registrado = True
+                    if not usuario.filter(fpa=data.get('fpa')).exists():
+                        new_user.save()
+                    else:
+                        return JsonResponse({'message':'Usuario ya registrado'},status=401)
+                else:
+                    return JsonResponse({'message':'Usuario no Habilidato para registrarse'},status=402)
+                
+                return JsonResponse({'message': 'Datos recibidos y guardados con éxito'},status=200)
+            
+            except json.JSONDecodeError:
+                return JsonResponse({'message': 'Error al decodificar el JSON'}, status=403)
+            
+        else:
+            return JsonResponse({'message': 'Tipo de contenido no válido'}, status=404)
+        
+    else:
+        return JsonResponse({'message': 'Método HTTP no válido'}, status=405)
